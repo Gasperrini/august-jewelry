74017d6f92b49a86104c9ea78cd88b9fcf94dd45
Ignas <ign.gasparavicius@gmail.com>
2020-05-05 19:42:59 +0300

    added products section

--- database/migrations/2020_05_04_182026_create_product_images_table.php
+++ database/migrations/2020_05_04_182026_create_product_images_table.php
@@ -16,7 +16,6 @@ class CreateProductImagesTable extends Migration
         Schema::create('product_images', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('product_id')->index()->unsigned();
-            $table->string('thumbnail')->nullable();
             $table->string('full')->nullable();
 
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
