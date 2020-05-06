8b25fc26f54f9289a06f3fa813e344fee4094fbd
Ignas <ign.gasparavicius@gmail.com>
2020-05-06 14:50:26 +0300

    added shopping cart

--- app/Http/Controllers/Site/ProductController.php
+++ app/Http/Controllers/Site/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Site;
 
+use Cart;
 use Illuminate\Http\Request;
 use App\Contracts\ProductContract;
 use App\Http\Controllers\Controller;
@@ -10,6 +11,7 @@ use App\Contracts\AttributeContract;
 class ProductController extends Controller
 {
     protected $productRepository;
+
     protected $attributeRepository;
 
     public function __construct(ProductContract $productRepository, AttributeContract $attributeRepository)
@@ -28,6 +30,11 @@ class ProductController extends Controller
 
     public function addToCart(Request $request)
     {
-        dd($request->all());
+        $product = $this->productRepository->findProductById($request->input('productId'));
+        $options = $request->except('_token', 'productId', 'price', 'qty');
+
+        Cart::add(uniqid(), $product->name, $request->input('price'), $request->input('qty'), $options);
+
+        return redirect()->back()->with('message', 'Item added to cart successfully.');
     }
 }
